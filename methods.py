3e6a726898c38a0323e68f49e9dd2a2c6446498a
Shailesh Bisht <65287730+peskyji@users.noreply.github.com>
2022-09-15 18:20:38 +0530

    Update methods.py

--- methods.py
+++ methods.py
@@ -74,11 +74,10 @@ def updating_progress_bar_while_downloading_audios(st, playlist, YouTube,py7zr, 
             st.sidebar.info(f"Converted - {filename}")
             latest_iteration.text(f'100% 1 of 1')
             bar.progress(100)
-            
-        except VideoUnavailable as e:
-            st.sidebar.error("Sorry😔. This video is unavailable to download")
         except Exception as e:
             st.exception(e)
+            if "videounavailable" str(e).lower():
+                st.sidebar.error("Sorry😔. This video is unavailable to download")
             st.stop()
     else:
         for i in range(0,100):
